f5c682a37dcf20b75ef1d38ef21abdb1a260cde8
Your Name <your_email@example.com>
2025-04-05 20:46:48 +0200

    Chat Repository created & updated

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/ChatParticipantRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.uzh.ifi.hase.soprafs24.repository;
-
-import ch.uzh.ifi.hase.soprafs24.entity.ChatParticipant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatParticipantRepository extends JpaRepository<ChatParticipant, Long> {
-}
-
