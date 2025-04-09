3d55b09d7eda74ac75f13c404ffba21fc601a145
darikzmn <ds.kazmina@gmail.com>
2025-04-09 23:23:43 +0200

    no change

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerTest.java
@@ -1,4 +1,4 @@
-// package ch.uzh.ifi.hase.soprafs24.controller;
+package ch.uzh.ifi.hase.soprafs24.controller;
 
 // import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
 // import ch.uzh.ifi.hase.soprafs24.entity.User;
