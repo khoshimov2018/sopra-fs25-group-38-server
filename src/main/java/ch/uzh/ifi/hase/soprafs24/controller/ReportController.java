c30b76ecacc77ec2ebb50af8cf446911eb6f3138
darikzmn <ds.kazmina@gmail.com>
2025-04-09 09:21:26 +0200

    Test Report functionalities and refactor ReportController to use ReportBlockService and ReportDTO

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/ReportController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/ReportController.java
@@ -1,43 +1,23 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
-import ch.uzh.ifi.hase.soprafs24.entity.Report;
-import ch.uzh.ifi.hase.soprafs24.entity.User;
-import ch.uzh.ifi.hase.soprafs24.repository.ReportRepository;
-import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.ReportDTO;
+import ch.uzh.ifi.hase.soprafs24.service.ReportBlockService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/report")
+@RequestMapping("/reports")
 public class ReportController {
 
-    private final ReportRepository reportRepository;
-    private final UserRepository userRepository;
+    private final ReportBlockService reportBlockService;
 
-    public ReportController(ReportRepository reportRepository, UserRepository userRepository) {
-        this.reportRepository = reportRepository;
-        this.userRepository = userRepository;
+    public ReportController(ReportBlockService reportBlockService) {
+        this.reportBlockService = reportBlockService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void reportUser(@RequestParam Long reporterId,
-                           @RequestParam Long reportedUserId,
-                           @RequestParam String reason) {
-        // Validate users
-        User reporter = userRepository.findById(reporterId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Reporter user not found"));
-        User reported = userRepository.findById(reportedUserId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Reported user not found"));
-
-        // Prevent duplicate reports
-        if (reportRepository.existsByReporterIdAndReportedUserId(reporterId, reportedUserId)) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "User already reported.");
-        }
-
-        // Save the report
-        Report newReport = new Report(reporter, reported, reason);
-        reportRepository.save(newReport);
+    public void createReport(@RequestBody ReportDTO reportDTO) {
+        reportBlockService.reportUser(reportDTO.getReporterId(), reportDTO.getReportedId(), reportDTO.getReason());
     }
 }
