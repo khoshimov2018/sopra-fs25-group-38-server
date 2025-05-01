009ea91fee19d45af0aeb92f83b15fc7e315ba57
Your Name <your_email@example.com>
2025-05-01 14:16:24 +0200

    fix the lasted commit

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/MessageRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/MessageRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository("messageRepository")
 public interface MessageRepository extends JpaRepository<Message, Long> {
@@ -13,6 +17,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     void deleteAllBySenderId(Long senderId);
 
     @Modifying
+    @Transactional
     @Query("DELETE FROM Message m WHERE m.channel.id = :channelId")
     void deleteByChannelId(@Param("channelId") Long channelId);
 }
