a0f84b5bc4efdedb6bffe25e6ede3a8649b0cefe
Your Name <your_email@example.com>
2025-05-11 17:10:11 +0200

    update for usertyping deletion

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/UserTypingStatusRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/UserTypingStatusRepository.java
@@ -3,6 +3,11 @@ package ch.uzh.ifi.hase.soprafs24.repository;
 import ch.uzh.ifi.hase.soprafs24.entity.UserTypingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
 
 public interface UserTypingStatusRepository extends JpaRepository<UserTypingStatus, Long> {
     Optional<UserTypingStatus> findByUserId(Long userId);
